6300b7603cc5e8127d1603078d8cf86c55b9955a
riturajpoot34 <riturajpoot014@gmail.com>
2024-05-28 21:52:27 +0530

    Add files via upload

---
+++ ans.1.cpp
@@ -0,0 +1,12 @@
+//1.Print "Hello KGI !!" on the console window.// ritu rajpoot
+
+#include <stdio.h>
+
+int main() {
+	
+      printf("Hello KGI !!\n");
+    
+    return 0;
+}
+	
+
---
+++ ans.5.cpp
@@ -0,0 +1,14 @@
+/* 
+5. Write a program to print “Happy Teachers Day” on the screen 
+(Remember to print double and single quotes).
+*/
+//ritu rajpoot
+
+#include <stdio.h>
+
+int main() {
+	
+    printf("\"Happy Teachers Day\"\n");
+    
+    return 0;
+}
